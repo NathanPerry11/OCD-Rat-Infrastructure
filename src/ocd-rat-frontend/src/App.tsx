7e586b1b0ab9e5f4a23345d7a4e337ccc314637b
Aidan Goodyer <aidangoodyer@gmail.com>
2025-11-10 11:26:54 -0500

    add query interface with mock response

--- src/ocd-rat-frontend/src/App.tsx
+++ src/ocd-rat-frontend/src/App.tsx
@@ -1,32 +1,32 @@
 import { Navbar01 } from "./components/navbar"
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { Footer } from "./components/Footer"
 import { Home } from "./pages/Home"
 
-import {NotFound} from "./pages/NotFound"
+import { Query } from "./pages/Query"
+import { NotFound } from "./pages/NotFound"
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-    <div className="relative w-full flex-grow">
+
       <title>RatBat 2</title>
       <Navbar01 />
 
-      <Routes>
-        <Route path="/" element ={<Home/>}/>
-         <Route path="/query" element ={<NotFound/>}/>
-            <Route path="/experiments" element ={<NotFound/>}/>
-            <Route path="/about" element ={<NotFound/>}/>
-            <Route path="*" element={<NotFound />} />
-      </Routes>
-      
+      <main className="flex-grow min-h-0">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/query" element={<Query />} />
+          <Route path="/experiments" element={<NotFound />} />
+          <Route path="/about" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+
+      <Footer />
 
     </div>
-
-<Footer></Footer>
-    
- </div>
   )
 }
 
